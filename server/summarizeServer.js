242c0593a5b0d17003decbd650ba6f7b323c4d9e
Kerwin <ktichmann@gmail.com>
2018-07-02 12:43:07 +0100

    added comments on summarizeServer

--- server/summarizeServer.js
+++ server/summarizeServer.js
@@ -10,12 +10,13 @@ const summarize = summarizer.summarize;
 const setupSummarize = summarizer.setupSummarize;
 const pathName = path.join(__dirname, "/public");
 
+//Sets up the summarizer - loading the corpus into the tf-idf library
 setupSummarize();
-
+//Set up Express
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(express.static("public"));
-
+//Accept all post requests, and respond with the three sentence summary
 app.post("*", (req,res) => {
   const url = req.body.url
 
@@ -25,20 +26,24 @@ app.post("*", (req,res) => {
       const result = {summary: "Error Fetching Summary!"}
       res.json(JSON.stringify(result))
     }
+    //Load the page html into cheerio
     $ = cheerio.load(body, {ignoreWhitespace: true});
+    //Remove all b and u tags (stage directions & character names)
     $("b").remove();
     $("u").remove();
+    //Use text within pre tag as script text
     let scriptText = $("pre").text();
+    //Replace all multiple spaces and new lines with single spaces
     scriptText = scriptText.replace(/\s[\s\n]+/g, " ");
+    //call Summarize on the sanitized script text to receive 3 sentence summary
     const result = {summary: summarize(scriptText)};
     res.json(JSON.stringify(result))
   })
 });
 
 app.get("*", (req, res) => {
-      res.sendFile("index.html", {"root": pathName})
+      res.json({"response": 404})
   })
-
 
 
 app.listen(3000, () => {console.log("test")})
