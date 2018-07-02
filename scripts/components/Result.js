d5d42f1b12a92fa8757307d127734ed657d5f06f
Kerwin <ktichmann@gmail.com>
2018-07-02 12:57:26 +0100

    Update Result.js

--- scripts/components/Result.js
+++ scripts/components/Result.js
@@ -5,12 +5,15 @@ import Loading from "./Loading"
 class Result extends React.Component{
   constructor(props){
     super(props);
+    //summary state to decide when summary has been received / when to stop loading and show response
     this.state = {
       summary: false
     }
   }
   componentDidMount(){
+    //Get the url from parent (App)
     let data = {url: this.props.url}
+    //handles any timeouts, by responding with an error if the promise doesn't resolve within the *time* argument
     const handleTimeout = (time, promise) => {
       return new Promise((res, rej) => {
         setTimeout(() => {
@@ -19,6 +22,8 @@ class Result extends React.Component{
         promise.then(res, rej)
         })
       }
+    //throws error if fetch request not resolved after 1 min
+    //Make fetch request to backend API
     handleTimeout(60000, fetch("https://movie-summary-api.herokuapp.com/", {
       body: JSON.stringify(data),
       method: "POST",
@@ -32,13 +37,14 @@ class Result extends React.Component{
     })
     .then((res) => {
       const summary = JSON.parse(res).summary;
+      //if we get a response - set state to the summary to "exit" out of loading, and show response
       if(summary){
         this.setState(() => {
           return {
             summary: summary
           }
         })
-      } else{
+      } else{ //if summary is empty, return error message
         this.setState(() => {
           return {
             summary: ["Error Fetching Summary, IMSDB Link May Be Broken"]
@@ -47,7 +53,7 @@ class Result extends React.Component{
       }
 
     })
-    .catch((err) => {
+    .catch((err) => { //catch any errors with error message
       this.setState(() => {
         return{
           summary: ["Error! Please Try Again"]
